650b1b5207b2034f478c0eebb410af4b64a44d48
Nathalya Pereira <84873348+nathalyapereira@users.noreply.github.com>
2023-01-30 00:06:24 -0300

    Hardskills

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/nathalyapereira/dio-portfolio/main/data/profile.json?token=GHSAT0AAAAAAB5DGHUSSSUJZ7SIO4VQSKL2Y6XFBSQ';
+    const url = 'https://raw.githubusercontent.com/nathalyapereira/dio-portfolio/main/data/profile.json?token=GHSAT0AAAAAAB5DGHUSO7ULW6AR57Y5JPDMY6XGP7Q';
     const fetching = await fetch(url)
     return await fetching.json();
 }
