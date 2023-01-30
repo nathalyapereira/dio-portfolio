7e376152937e14761bf705db5563899a5179b6b3
Nathalya Pereira <84873348+nathalyapereira@users.noreply.github.com>
2023-01-30 00:24:43 -0300

    Hardskills

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/nathalyapereira/dio-portfolio/main/data/profile.json?token=GHSAT0AAAAAAB5DGHUSO7ULW6AR57Y5JPDMY6XGP7Q';
+    const url = 'https://raw.githubusercontent.com/nathalyapereira/dio-portfolio/main/data/profile.json?token=GHSAT0AAAAAAB5DGHUSTOENQAARTUXGTTM6Y6XHAUQ';
     const fetching = await fetch(url)
     return await fetching.json();
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -26,8 +26,13 @@ function updateProfileInfo(profileData) {
 function updateSoftSkills(profileData) {
     const softSkills = document.getElementById('profile.skills.softSkills')
 
-   softSkills.innerHTML = profileData.skills.softSkills.map((Skill) => ` <li>${Skill}</li>`).join('')
+    softSkills.innerHTML = profileData.skills.softSkills.map((skill) => ` <li>${skill}</li>`).join('')
+}
 
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+
+    hardSkills.innerHTML = profileData.skills.hardSkills.map((skill) => ` <li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}">`).join('')
 }
 
 (async () => {
@@ -35,5 +40,6 @@ function updateSoftSkills(profileData) {
     const profileData = await fetchProfileData ()
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
+    updateHardSkills(profileData)
 
 })()
