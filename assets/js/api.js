1f271164dd2d734bdd24c90ec8f85c94b183fe32
Nathalya Pereira <84873348+nathalyapereira@users.noreply.github.com>
2023-01-30 16:26:07 -0300

    Experiêcias

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/nathalyapereira/dio-portfolio/main/data/profile.json?token=GHSAT0AAAAAAB5DGHUSTOENQAARTUXGTTM6Y6XHAUQ';
+    const url = 'https://raw.githubusercontent.com/nathalyapereira/dio-portfolio/main/data/profile.json?token=GHSAT0AAAAAAB5DGHUTXONESY4QZHW5N7UOY6YDGTQ';
     const fetching = await fetch(url)
     return await fetching.json();
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -35,11 +35,47 @@ function updateHardSkills(profileData) {
     hardSkills.innerHTML = profileData.skills.hardSkills.map((skill) => ` <li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}">`).join('')
 }
 
+function updateLanguages(profileData) {
+    const languages = document.getElementById('profile.languages')
+    languages.innerHTML = profileData.languages.map((language) => `<li>${language}`).join('')
+}
+
+function updatePortfolio(profileData) {
+    const portfolio = document.getElementById('profile.portfolio')
+    portfolio.innerHTML = profileData.portfolio.map((project) => {
+         
+    return `
+    <li>
+    <h3 ${project.github} ? 'class="github"' : ''>${project.name}</h3>
+    <a href="${project.url}" target="_blank">${project.url}</a>
+    </li>
+`
+}).join('')
+    
+}
+
+function updateProfessionalExperience(profileData) {
+    const professionalExperience = document.getElementById('profile.professionalExperience')
+    professionalExperience.innerHTML = profileData.professionalExperience.map((experience) => {
+        return `
+            <li>
+                <h3 class="title">${experience.name}</h3>
+                <p class="period">${experience.period}</p>
+                <p>${experience.description}</p>
+            </li>
+        `
+    }).join('')
+
+}
+
+
 (async () => {
 
     const profileData = await fetchProfileData ()
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
     updateHardSkills(profileData)
-
+    updateLanguages(profileData)
+    updatePortfolio(profileData)
+    updateProfessionalExperience(profileData)
 })()
