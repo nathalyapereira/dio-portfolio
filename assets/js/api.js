5dbbd77abf2f5fbf9cf36162369a7d9aa688f3e3
Nathalya Pereira <84873348+nathalyapereira@users.noreply.github.com>
2023-01-29 23:03:56 -0300

    Softskills

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/nathalyapereira/dio-portfolio/main/data/profile.json?token=GHSAT0AAAAAAB5DGHUS5IHXP424BPBEEBWKY6W6CMA';
+    const url = 'https://raw.githubusercontent.com/nathalyapereira/dio-portfolio/main/data/profile.json?token=GHSAT0AAAAAAB5DGHUSR656EY4AQCA6W2VQY6W7VPQ';
     const fetching = await fetch(url)
     return await fetching.json();
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ function updateProfileInfo(profileData) {
     const photo = document.getElementById('profile.photo')
     photo.src = profileData.photo
     photo.alt = profileData.name
+    
     const name = document.getElementById('profile.name')
     name.innerText = profileData.name
 
@@ -24,7 +25,7 @@ function updateProfileInfo(profileData) {
 
 (async () => {
 
-    const profileData = await fetchProfileData()
+    const profileData = await fetchProfileData ()
     updateProfileInfo(profileData)
 
 })()
